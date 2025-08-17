a8d3a696ea5703c33d6870286023862cda2e0f1e
Ganesh Pataiya (UI Developer ) <38519608+ganeshpataiya@users.noreply.github.com>
2025-08-17 22:33:49 +0530

    updated code pushed

--- src/app/layout/bgsetting/bgsetting.ts
+++ src/app/layout/bgsetting/bgsetting.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, Renderer2 } from '@angular/core';
+import {  Component, Inject, Renderer2, PLATFORM_ID } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-bgsetting',
@@ -8,38 +9,40 @@ import { Component, Renderer2 } from '@angular/core';
   styleUrl: './bgsetting.scss'
 })
 export class Bgsetting {
-constructor(private renderer: Renderer2) {}
+    constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
 
- setBackground(type: 'white' | 'dark' | 'gradient' | 'image') {
-  const body = document.body;
- this.renderer.removeClass(body, 'white-bg');
-    this.renderer.removeClass(body, 'dark-bg');
-    this.renderer.removeClass(body, 'gradient-bg');
-    this.renderer.removeClass(body, 'image-bg');
-     // Add new background class
-    this.renderer.addClass(body, `${type}-bg`);
-  
- }
-// background video
-  // activeDiv: string | null = null;
-  //   showDiv(divId: string) {
-  //   this.activeDiv = this.activeDiv === divId ? null : divId;
-  // }
+  setBackground(type: 'green' | 'red' | 'blue' | 'orange') {
+    if (!isPlatformBrowser(this.platformId)) return; // SSR-safe
+    const body = this.document.body;
 
-   ngOnInit() {
+    // remove previous color classes
+    ['green','red','blue','orange'].forEach(c =>
+      this.renderer.removeClass(body, `color-${c}`)
+    );
+
+    // add new one
+    this.renderer.addClass(body, `color-${type}`);
+  }
+
+
+  ngOnInit() {
     this.showDiv('video1'); // ✅ Default visible div
   }
-  
- showDiv(videoId: string) { 
-  const videos = ['video1', 'video2', 'video3'];
-  videos.forEach(id => {
-    const videoElement = document.getElementById(id);
-    if (videoElement) {
-      this.renderer.setStyle(videoElement, 'display', id === videoId ? 'block' : 'none');
-    }
-  });
- }
- 
- 
+
+  showDiv(videoId: string) {
+    const videos = ['video1', 'video2', 'video3'];
+    videos.forEach(id => {
+      const videoElement = document.getElementById(id);
+      if (videoElement) {
+        this.renderer.setStyle(videoElement, 'display', id === videoId ? 'block' : 'none');
+      }
+    });
+  }
+
+
 }
